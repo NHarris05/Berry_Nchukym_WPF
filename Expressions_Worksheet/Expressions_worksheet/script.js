87b00c0ef7a129899d3bbb5f528045ce5ea8d125
Nchukym_Harris <nlberry_fullsail.edu>
2015-04-12 14:38:28 -0400

    Assigned var and tested the script.
    The first script was to assign and effectively test the console to
    ensure the current age of sparky was being printed to the console. I
    assigned var to both current human age, and dog years and added the dog
    name. Next I tested the script by running it on the console. It was
    effective. The current age was a product of current age (human years)
    and dog years.

--- Expressions_Worksheet/Expressions_worksheet/script.js
+++ Expressions_Worksheet/Expressions_worksheet/script.js
@@ -8,4 +8,18 @@
 
 //alert("What is your Name?");//
 
+var dogname = "sparky"; //assigning the name to the console//
+console.log(dogname);
 
+var sparkyAge = "14";//assigning an age to the console.//
+console.log(sparkyAge);
+
+var dogYears = 7;
+console.log(dogYears); //this is 7 years more than 1 year in human age//
+
+var result = (sparkyAge * dogYears); //I multiplied var age * var dogYears to get sparkys current age//
+
+console.log(result);
+
+
+//I tested my script to ensure it is functioning and properly printing what it should have.//
