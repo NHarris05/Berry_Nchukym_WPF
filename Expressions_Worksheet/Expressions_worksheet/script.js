e2dafce3e29b13408e2ef8e442282413587b44ef
Nchukym_Harris <nlberry_fullsail.edu>
2015-04-12 12:23:48 -0400

    Created New folders
    Created required folders for class. One for videos which include all
    lectures up until this point. Then I created the expression_worksheet
    and tested it with an alert. The alert was then commented out.

---
+++ Expressions_Worksheet/Expressions_worksheet/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by nchukymharris on 4/12/15.
+ * Expressions_worksheet
+ */
+
+
+//Dog years finding sparky's age //
+
+//alert("What is your Name?");//
+
+
--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,98 @@
 /**
  * Created by nchukymharris on 4/3/15.
  */
+/** notes to self*/
+
+alert("WHAT IS YOUR NAME?");
+/*
+var yearBorn=1955;
+console.log("Hello World");
+console.log(yearBorn);
+*/
+var age;//declaration
+    age = 27; //definition
+
+//storage container = value
+
+var yearOfBirth= 1988;//declaring and defining on the same line
+
+//*VRIABLES OF Different Types, Numbers,Booleans and Strings*//
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "Kym";
+
+//Arrays
+
+var muppetNames = ["Kermit","Gonzo","Piggy","RAT" ];//0,1,2,3
+
+console.log(muppetNames[0]);
+//Expersions
+
+var a = 2;//Sets up variable
+a = a + 3;
+console.log(a);
+ var yearBorn = 1988;
+var age = 2011 - yearBorn;
+console.log(age);
+
+//Expressions-Arithmetic Operators
+
+var width = 4;
+var height = 8;
+var area = width *height/2;
+console.log(area)
+
+//Expressions-Modulo Operator
+
+var remainder = 32/10;
+console.log(remainder);
+//PEMDAS
+
+var quiz1 = 90;
+var quiz2 = 30;
+var quiz3 = 88;
+var quiz4 = 100;
+
+var average = (quiz1 +quiz2 +quiz3 +quiz4)/4;
+console.log(average);
+//console.log(average)
+var length = 7;
+var width = 6;
+var perimeter = (length *2) + (width *2);
+//casting
+var stringVar = "6";
+var result = 7 + Number(stringVar);
+console.log(result);
+
+var areaCode = 518;
+var firstPart = 878;
+
+var secPart = 4536;
+
+
+//Expressions -Assignment Operators
+
+var a = 3;
+a += 4;
+console.log(a);
+
+//Promt and Alert
+
+var userInput = prompt("Enter your name");
+console.log(userInput);
+
+var width = prompt ("We are calculating the area of a rectangle.\nPlease enter the width");
+var height = prompt ("Please enter the height");
+var area = width * height;
+//console.log(area);//
+alert("The area of your rectangle is" + area);
+
+
+
+
+var phoneNo = String(areaCode) +firstPart + secPart;
+console.log(phoneNo)
+;
+
+
