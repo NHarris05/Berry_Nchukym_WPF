36415a6bb02119ba0a71921ff8b1db4782c2c8ce
Nchukym_Harris <nlberry_fullsail.edu>
2015-04-13 21:58:46 -0400

    Expressions Assignment created and tested
    Expressions assignment created. Html and js were created and linked. I
    named the html page as expressions assignment, and linked the js sheet.
    Next I added an alert to the JS and tested it. It worked.

---
+++ Expressions_Worksheet/Expressions_Assignment/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nchukymharris on 4/13/15.
+ */
+
+
+//prompt = alert ("Whats Your Name");// Testing console to make sure the alert is working. JS is accurately linked and functioning.
+
+
