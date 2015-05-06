6429e341a3b78f17fe81fd42ce4665de180631e1
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-06 11:03:32 -0400

    Created a Muppet array
    I followed the screen cast for setting and creating arrays.

--- AssociatingJS 5/js/script.js
+++ AssociatingJS 5/js/script.js
@@ -16,7 +16,7 @@ age = 38; //definition
 var yearOfBirth = 1970; //declaring and defining on the same line*/
 
 
-var
+/*var
 var age = 38;
 var
 var kermit = "my friend";
@@ -25,7 +25,16 @@ var isStudent = false;
 var
 var firstName = "Kermit";
 var phrase;
-phrase = 'I don\'t know';
+phrase = 'I don\'t know';*/
+
+/* Arrays*/
+
+var muppetNames = ["Kermit","Mrs Piggy","Gonzo","Rizzo"]; //index 0,1,2,3
+console.log(muppetNames[0]);
+
+
+
+
 
 
 
