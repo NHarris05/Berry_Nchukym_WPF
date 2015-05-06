1576f2a4afafc7e8466d59e0ef71fc38e5ca0d23
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-06 18:09:32 -0400

    Slice Of Pie expressions worksheet
    I completed the slice of pie portion of the worksheet by declaring
    variables which would define the pies ordered, slices each pie had, and
    the amount of guest. I used arithmetic expressions to figure out how
    many slices each guest had and had a remainder. Everything is commented
    out, tested in the console and then printed out onto an alert to the
    screen.

--- Expressions_worksheet/script.js
+++ Expressions_worksheet/script.js
@@ -11,5 +11,21 @@ console.log(humanAge); //Printed this to console to make sure it was accurate
 alert("Sparky is" + dogAge + "Which is" + humanAge + "In Human Years"); //The alert
 was what printed up the entire equation in sentence form*/
 
+/** Nchukym Harris
+ * Slice of Pie part 1
+ */
+
+
+var pies = "5";//declaration of the amount of pies ordered
+var slices = "12";//each pie has 12 slices
+var totalSlices = (pies * slices);//the total amount of slices = multiply pie times slices
+ console.log(totalSlices);//result to print to the console
+
+var guest = 23;//the amount of guests who attended
+var result = totalSlices / guest;//figures out how many slices each guest had divide slices by guest
+console.log(result);
+
+alert("Each guest had" + result + "Slices of pizza \ at the party");//Alert that prints the total amount of slices to the screen
+
 
 
