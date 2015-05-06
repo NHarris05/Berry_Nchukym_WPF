df32fd5f4c8de0902d5137a46201575af6af930a
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-06 17:33:34 -0400

    First part of expressions worksheet complete
    The first problem of the expressions worksheet is done. I used comments
    for each step and set var for both dog age and human age. I also used
    an alert to print the entire sentence to a pop up after I checked it in
    the console.log.

---
+++ Expressions_worksheet/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by nchukymharris on 5/6/15.
+ */
+//Dog Years//
+
+/*var dogAge = 13; //defining the age of the dog
+
+var humanAge = 7 * 13; // var humanAge is 7 times the dogAge
+console.log(humanAge); //Printed this to console to make sure it was accurate
+
+alert("Sparky is" + dogAge + "Which is" + humanAge + "In Human Years"); //The alert
+was what printed up the entire equation in sentence form*/
+
+
+
