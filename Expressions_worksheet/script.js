f1f58382ae2f41b346088ad4f1defb4430a2c2d1
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-06 21:16:41 -0400

    Created a weekly budget average
    I completed the average shopping bill assignment from the expressions
    worksheet. Using an array, I created a var to add the array using the
    index numbers. Then I checked it in the log to make sure it printed.
    Next I created an alert which printed this information to the screen in
    a dialog box. I also did the same for the average weekly bill using the
    division operator.

--- Expressions_worksheet/script.js
+++ Expressions_worksheet/script.js
@@ -1,6 +1,5 @@
-/**
- * Created by nchukymharris on 5/6/15.
- */
+/*Created by nchukymharris on 5/6/15.*/
+
 //Dog Years//
 
 /*var dogAge = 13; //defining the age of the dog
@@ -27,14 +26,27 @@ console.log(result);
 
 alert("Each guest had" + result + "Slices of pizza \ at the party");//Alert that prints the total amount of slices to the screen
 
-//* Nchukym Berry Slice of Pie part 2*//
+//* Nchukym Berry Slice of Pie part 2*/
 /*var sparkyPiece = 40 % 13;//the amount of total slices divided by the guests plus sparky
 console.log(sparkyPiece); //printed to the console is the equation
 alert("Sparky Got To Have" + sparkyPiece + "Slices Of Pizza");//this is the alert the waill print to the screen*/
 
 //* Nchukym Berry Average shopping bill*//
 
-var bill = [99, 59, 103, 88, 212];
-var result = bill[0] + bill[1] + bill[2] + bill[3] + bill[4];
-console.log(result);
+var bill = [99, 59, 103, 88, 212];//each weeks money spent on groceries
+var result = bill[0] + bill[1] + bill[2] + bill[3] + bill[4]; //adding all the bills up
+console.log(result); //printing to the console
+
+alert("You have spent" + result + "$" + "On \n groceries this month"); //This is the total amount spent over the month printed to the screen as an alert
+
+var average = result/5; //divide the result by the amount of weeks to get the weekly spending estimate
+
+console.log(average); //print average to the console
+
+alert("You spend"+ average + " \n On groceries each week"); //this is the average money spent per week printed to the screen.
+
+
+
+
+
 
