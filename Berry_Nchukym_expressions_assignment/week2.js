a1908951becb92df7d17c0a7ec6d1b6b46e129f0
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-07 09:51:23 -0400

    Created the expressions assignment and have began coding
    This calculator is for nurses to see if they are being overworked. I
    have created the js and html file and tested it with an alert to make
    sure it was running properly. I then created some user prompts to input
    information. The next step was to output the days the user entered,
    followed by a prompt question verifying the input. I am now working on
    hour calculations. there is an array using numbers which will calculate
    the amount of hours worked per day, and per month.

---
+++ Berry_Nchukym_expressions_assignment/week2.js
@@ -0,0 +1,48 @@
+/* Nchukym Berry
+5/7/2015
+sec-02
+Expressions Assignment
+ */
+
+alert("This is a calculator for nurses \n  to see if you are \n  being over worked \n Please enter the days and hours you work each week");
+
+/*var a = prompt("Enter The first  \n day you work in the week");
+console.log(a);
+
+var b = prompt ("How many hours \n do you work on this day?");
+console.log(b);
+
+var c = prompt("Enter the second day you work thi week");
+console.log(c);
+
+var d = prompt("How many hours do you \n work on that day?");
+console.log(d);
+
+var e = prompt ("Enter the last few days that you work \n in a week");
+console.log(e);
+
+var f = prompt ("How many hours do you \n work on that day?");
+console.log(f);
+
+
+var workDays = [ "Monday","Tuesday", "Friday", "Saturday", "Sunday"];
+alert("You work" + workDays + "each week?");
+var answer = prompt("True or false");
+console.log(answer);*/
+
+
+
+
+var hoursWorked = [12, 16, 18,24, 24];
+var hoursADay = hoursWorked[0] + hoursWorked[1] + hoursWorked[2] + hoursWorked[3] + hoursWorked[4] / 5;
+console.log(hoursADay);
+
+var hoursMonthly = hoursWorked[0] + hoursWorked[1] + hoursWorked[2] + hoursWorked[3] + hoursWorked[4] * 4;
+console.log(hoursMonthly);
+
+
+
+
+
+
+
