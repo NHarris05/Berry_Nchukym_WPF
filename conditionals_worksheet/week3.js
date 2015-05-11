54e312f9f2e90dd1de1044df290c1179499e009b
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-11 09:57:44 -0400

    Week 3 Conditionals Worksheet first group completed
    I completed the celsius to fahrenheit assignment. What I did was set
    two variables. One for each temperature and degree units. Next I
    created an if and else statement which would determine which message to
    print to the console. Each step is commented with an explanation of
    what I am doing. I also tested everything to make sure the code is
    running properly.

--- conditionals_worksheet/week3.js
+++ conditionals_worksheet/week3.js
@@ -2,4 +2,24 @@
  * Created by nchukymharris on 5/8/15.
  */
 
-alert("Hello World!");
+//Celsius to Fahrenheit converter//
+
+//degrees in F or C (declaring var)
+var degrees = "95F"; //Degrees in fahrenheit.. So If it is 95 degrees outside
+var optionDegree = "35C"; //Degrees in Celsius... If it is 35 degrees C
+
+
+    if(degrees < optionDegree) { //If the degrees is less than the option degree
+        console.log("You can wear shorts today!"); // Print to console you can wear shorts today if the statement is false
+
+    }else {
+        console.log("You might want to bundle up!"); //The else will print if the statement degrees < optiondegree is true
+    }
+
+
+
+
+
+
+
+
