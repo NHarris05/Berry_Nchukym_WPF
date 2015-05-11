8dd0d5614a8aedbf4ee377ae36f99462b81855ff
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-11 10:37:00 -0400

    Created and tested the user input portion of the assignment week 3
    I created a code for the user input portion of week 3. I declared
    variables for username and passwords as well as two prompts for
    username and passwords. I then set one equal to and not equal to and
    ran the script to make sure it was working properly. Everything is
    commented appropriately.

--- conditionals_worksheet/week3.js
+++ conditionals_worksheet/week3.js
@@ -5,7 +5,7 @@
 //Celsius to Fahrenheit converter//
 
 //degrees in F or C (declaring var)
-var degrees = "95F"; //Degrees in fahrenheit.. So If it is 95 degrees outside
+/*var degrees = "95F"; //Degrees in fahrenheit.. So If it is 95 degrees outside
 var optionDegree = "35C"; //Degrees in Celsius... If it is 35 degrees C
 
 
@@ -14,10 +14,28 @@ var optionDegree = "35C"; //Degrees in Celsius... If it is 35 degrees C
 
     }else {
         console.log("The Temperature is" + optionDegree); //The else will print if the statement degrees < optiondegree is true
-    }
+    }*/
 
 //Nchukym Harris
 //check the log in//
+
+ var userName = "Kym"; //The correct username that will opt the print ot message
+var password = "apple"; //The correct password that will print out the second console message
+
+var fakeUsername = prompt("ENTER YOUR USERNAME"); //create a prompt which will ask for a username. I then input a pake username
+var fakePw = prompt ("ENTER YOUR PASSWORD"); // create a fake password input.
+
+if(userName === fakeUsername){
+     console.log("User Not Found. Try Again");// if the user name is less than the fake user name than print this message
+
+}if(password != userName) {
+    console.log("Password Does Not Match Our Records "); //if the pw is greater than the user name than print this message
+}else{
+    console.log("Welcome Kym!"); // otherwise print this message
+
+}
+
+
 
 
 
