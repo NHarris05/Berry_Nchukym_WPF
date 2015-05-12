feafb0638a5bd0907070016b985d4bcd00840fa1
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-12 10:10:48 -0400

    Created a name and two prompts for my calculator
    I created a calculator with will serve for the purpose of calculating
    seeds planted for the spring, and will estimate the time in which the
    reaping will take place. This first part is just two prompts that have
    been casted to create a total number of seeds planted.

--- Berry_Nchukym_Conditionals_Assignment/Conditionalsscript.js
+++ Berry_Nchukym_Conditionals_Assignment/Conditionalsscript.js
@@ -1,5 +1,12 @@
 /**
  * Created by nchukymharris on 5/11/15.
  */
+// sec-02
+//Conditionals assignment.... Growing carrots and onions
 
-alert("Hello world");
+
+var growCarrot = prompt("How Many Carrot Seeds Did You Plant?"); // The first prompt will have the user input the amount of carrot seeds they planted this spring
+var growOnion = prompt ("How Many Onion Seeds Did You Plant?"); // This prompt will ask for the amount of onion seed the user planted this spring
+  console.log(Number(growCarrot) + Number(growOnion)); //The result will print in the console as a casted number. Because the numbers for each were 50 and 50, the result without casting would have been 5050 as the amount of seeds total planted
+
+
