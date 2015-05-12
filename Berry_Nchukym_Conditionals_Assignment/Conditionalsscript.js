bd8cd988d7567a7b49b03252425b049fd3feee45
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-12 11:05:00 -0400

    Added a mathematical element to the calculator
    I added a mathematical element to the calculator as well as added an
    else if statement. I will be using the mathematical element more so in
    this calculator to demonstrate my knowledge of this weeks class.

--- Berry_Nchukym_Conditionals_Assignment/Conditionalsscript.js
+++ Berry_Nchukym_Conditionals_Assignment/Conditionalsscript.js
@@ -8,11 +8,13 @@
 var growCarrot = prompt("How Many Carrot Seeds Did You Plant?"); // The first prompt will have the user input the amount of carrot seeds they planted this spring
 var growOnion = prompt ("How Many Onion Seeds Did You Plant?"); // This prompt will ask for the amount of onion seed the user planted this spring
   console.log(Number(growCarrot) + Number(growOnion)); //The result will print in the console as a casted number. Because the numbers for each were 50 and 50, the result without casting would have been 5050 as the amount of seeds total planted
-
+var width = 25;
+var length = 72;
 //Now that I have the number of seeds planted, what I will do next is created two variables which will determine when the reaping will happen based on when the seeds were sown.
-
+//The length and width will determine how far apart the seeds will be from each other.
 var month = "april";
 var season = "summer";
+var plantedSeeds = false;
 
 if(month ){
     console.log("You Should Expect a Harvest In" + " " + [30 -1 + 75] +" " + "Days From Now" ); //If the user planted their seed in april during the beginning of the month, then the console will print out an estimated time... exact days until the harvesting time
@@ -23,7 +25,10 @@ if(month ){
 if(season){
     console.log("Your harvest will be ready on September 15th");// This is the time the user will be able to pick their harvest if they planted their crop in june.
 
-}else{
+}else if(plantedSeeds){
     console.log("Better luck next year!"); //The message the user will see if they just did not plant anything until after june. This is assuming they live in NY, and with the weather here, harvesting a good and consumable crop in dec or any of the cold months is close to impossible. The soil lacks fertility enough in the hot seasons.
-
+}else{
+      console.log("Better Luck next year!");
 }
+
+
