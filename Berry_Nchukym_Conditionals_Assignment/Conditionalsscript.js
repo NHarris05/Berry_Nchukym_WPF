91ab9f39135be6522e2ab8571d532ae590767242
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-12 14:11:35 -0400

    Created concatenating strings, with 1 alert and two prompts
    In order to test this calculator and make sure it is working, I created
    two more prompts instructing the user to input the width and length of
    their raised bed. The result was printed out to the console and an
    alert was created on screen. I also added some more comments where they
    were lacking to better explain the functionality of the calculator.

--- Berry_Nchukym_Conditionals_Assignment/Conditionalsscript.js
+++ Berry_Nchukym_Conditionals_Assignment/Conditionalsscript.js
@@ -8,21 +8,37 @@
 var growCarrot = prompt("How Many Carrot Seeds Did You Plant?"); // The first prompt will have the user input the amount of carrot seeds they planted this spring
 var growOnion = prompt ("How Many Onion Seeds Did You Plant?"); // This prompt will ask for the amount of onion seed the user planted this spring
   console.log(Number(growCarrot) + Number(growOnion)); //The result will print in the console as a casted number. Because the numbers for each were 50 and 50, the result without casting would have been 5050 as the amount of seeds total planted
-var width = 25;
-var length = 72;
+//this part of the calculator will determine the area of the raised bed. This will be a factor in how far the seeds should be spread apart
+
+
+
+
+var width = prompt ("Enter Your Raised Bed Width");
+var length = prompt ("Enter Your Raised Bed Length");
+ var area = width * length;
+console.log(area + " " + "square feet");
+alert("The Area of Your \n Raised Bed Is" +  " " + area + "sq ft");
+//The alert to communicate with the user of what the area is.. a string of text with the area result.
+
+
+//printing to the console the area of the rectangle plus the concatenated string for unit
+
+
 //Now that I have the number of seeds planted, what I will do next is created two variables which will determine when the reaping will happen based on when the seeds were sown.
 //The length and width will determine how far apart the seeds will be from each other.
+
+
 var month = "april";
 var season = "summer";
 var plantedSeeds = false;
 
 if(month ){
     console.log("You Should Expect a Harvest In" + " " + [30 -1 + 75] +" " + "Days From Now" ); //If the user planted their seed in april during the beginning of the month, then the console will print out an estimated time... exact days until the harvesting time
-  alert();
+
 }else{
   console.log("You should expect a early September/ late October harvest"); //if they planted the seeds in the summer months/ june than they will see a message that they should expect a harvest in the beginning of September/ late October
-}
-if(season){
+
+}if(season){
     console.log("Your harvest will be ready on September 15th");// This is the time the user will be able to pick their harvest if they planted their crop in june.
 
 }else if(plantedSeeds){
