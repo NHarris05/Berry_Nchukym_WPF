18298ea042739b4e91889efceaf02e6ad9d3ba79
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-27 11:19:32 -0400

    Changed subscribers to number of videos made per day, also added comments and adjusted the first half
    I added videos per day and added more comments. I also adjusted numbers
    on the first part to reflect passing and checked on my calculator to
    make sure it was working correctly.

--- Berry_Nchukym_Functions_Assignment/script.js
+++ Berry_Nchukym_Functions_Assignment/script.js
@@ -4,17 +4,28 @@
  * YouTube Earnings
  */
 
- prompt("How Many YouTube Subscribers Do You Have?");//The first user prompt,I will input 62.
+ prompt("How Many YouTube Videos Do You Make Each Week?");//The first user prompt,I will input 10.
 //var i set for subscribers
- prompt ("How Many viewers Does your Channel Have?"); // The second user prompt will instruct the user to input the amount of views their channel has. I will input 13,423
+ prompt ("How Many viewers Does your Channel Have A Day?"); // The second user prompt will instruct the user to input the amount of views their channel has. I will input 13,423
 // var v is set for viewers
 
 
-revenueEstimate(62, 13423); //revenueEstimate is set to hold the viewers and subcribers
+//revenueEstimate(10, 13423); //revenueEstimate is set to hold the videos made and subscribers.
 
-   function revenueEstimate (i,v){ //main function is going to output the information
-       var revenue = i * v; //the viewers and subscribers are going to be multiplied to work with the next part which will calculate the estimated earnings.
+   function revenueEstimate (i){ //main function is going to output the information
+       var revenue = i * 13423; //the viewers and subscribers are going to be multiplied to work with the next part which will calculate the estimated earnings.
+    //I got  134230  as a result after I input 10 in the user prompt
      console.log(revenue);
    }
+revenueEstimate(20);//answer was 268460 after passing
+revenueEstimate(15);// answer was 201345 after passing/ also checked on my calculator
+
+ monthlyEarnings(134230,.25,2.80);
+
+function monthlyEarnings(v,cpm,budget){
+     var estimateEarnings = (v * cpm / budget);// I got 11984.82142857143 for a result
+    console.log( "Your Estimated Monthly YouTube Earnings is" + " " + estimateEarnings);
+}
+
 
 
