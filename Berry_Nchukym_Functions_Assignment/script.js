282dbd567a700de1b0df59854866d5ce31fe7cb7
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-27 09:43:32 -0400

    Created the functions assignment file
    I created the functions assignment file and tried creating a directory
    for the JS file.

---
+++ Berry_Nchukym_Functions_Assignment/script.js
@@ -0,0 +1,5 @@
+/**
+ * Created by nchukymharris on 5/27/15.
+ */
+
+alert("Hello World!");
--- Week4Loops/java.js
+++ Week4Loops/java.js
@@ -2,3 +2,23 @@
  * Created by nchukymharris on 5/26/15.
  */
 
+//* loops *//
+
+var b = 10; //sets the index
+while (b > 0){//checks the condition
+    console.log(b + ' kegs on the wall');
+    b--; //increments or decrements the index
+}
+console.log('-------- Do While Loops------------------');
+
+var c = 10;
+
+ do{
+    console.log(c + 'kegs on the wall');
+    c--;
+}
+ while(c > 0);
+
+for(var i = 10; i > 0; i--);{
+    console.log(i + 'kegs on the wall');
+}
