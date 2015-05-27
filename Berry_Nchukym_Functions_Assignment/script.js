abc863326b48dacd707861699c1440614e400115
Nchukym_Harris <nlberry_fullsail.edu>
2015-05-27 10:32:31 -0400

    First part of the calculator set up with an index wich will be used throughout the calculator
    I created the first part of the calculator.  This holds the viewers and
    the subscribers a YouTube channel has. This first part will divide the
    subscribers parameter from the viewers parameter to get the first part
    of the earnings estimate.

--- Berry_Nchukym_Functions_Assignment/script.js
+++ Berry_Nchukym_Functions_Assignment/script.js
@@ -2,4 +2,16 @@
  * Created by nchukymharris on 5/27/15.
  */
 
-alert("Hello World!");
+ prompt("How Many YouTube Subscribers Do You Have?");//The first user prompt,I will input 62.
+//var i set for subscribers
+ prompt ("How Many viewers Does your Channel Have?"); // The second user prompt will instruct the user to input the amount of views their channel has. I will input 13,423
+// var v is set for viewers
+
+
+revenueEstimate(62, 13423); //revenueEstimate is set to hold the viewers and subcribers
+
+   function revenueEstimate (i,v){ //main function is going to output the information
+       var revenue = i / v; //the viewers and subscribers are going to be divided to work with the next part which will calculate the estimated earnings.
+     console.log(revenue);
+   }
+
